da6377888649a96a353f0850623058277aafe33e
MaheshSundaramurthy <mahesh.sundaramurthy@touchqlik.com>
2020-01-29 19:03:31 -0500

    Updates

---
+++ precache-manifest.f086300b6798320465cb6efe84d8f095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9116d2b30a4ecc770532f8b03ac2470d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80036fa47e948d7e7948",
+    "url": "/monsters-rolodex/static/css/main.a53cabdc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80036fa47e948d7e7948",
+    "url": "/monsters-rolodex/static/js/main.1bb6ac25.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
